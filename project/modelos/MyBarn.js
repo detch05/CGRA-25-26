44894cc0dfe72fca40e6e12b8ac984c19edab86e
Marcelo Lima <up202305892@up.pt>
2026-05-23 17:03:03 +0100

    versão final de barn

--- project/modelos/MyBarn.js
+++ project/modelos/MyBarn.js
@@ -22,6 +22,7 @@ export class MyBarn extends CGFobject {
         this.roofMaterial.setDiffuse(0.6, 0.4, 0.3, 1);
         this.roofMaterial.setSpecular(0.18, 0.15, 0.12, 1);
         this.roofMaterial.setShininess(40);
+        
         // Door material
         this.doorMaterial = new CGFappearance(scene);
         this.doorMaterial.loadTexture("./images/door.jpg");
@@ -29,6 +30,15 @@ export class MyBarn extends CGFobject {
         this.doorMaterial.setDiffuse(0.9, 0.9, 0.9, 1);
         this.doorMaterial.setSpecular(0.1, 0.1, 0.1, 1);
         this.doorMaterial.setShininess(10);
+
+        // Window material
+        this.windowMaterial = new CGFappearance(scene);
+        this.windowMaterial.loadTexture("./images/window.jpg");
+        this.windowMaterial.setAmbient(0.4, 0.3, 0.25, 1);
+        this.windowMaterial.setDiffuse(0.9, 0.9, 0.9, 1);
+        this.windowMaterial.setSpecular(0.1, 0.1, 0.1, 1);
+        this.windowMaterial.setShininess(10);
+        
     }
 
     display() {
@@ -39,11 +49,8 @@ export class MyBarn extends CGFobject {
         this.scene.scale(4, 3, 1);
         this.woodMaterial.apply();
         this.Quad.display();
-        // door (relative to this front quad)
         this.scene.pushMatrix();
-        // place slightly in front to avoid z-fighting
         this.scene.translate(0.165, 0, 0.02, 0.01);
-        // door size: width 1, height 1.8 (wall scaled 4x3)
         this.scene.scale(0.7, 0.8, 0);
         // set UVs so texture fills the door quad
         this.Quad.updateTexCoords([0,1, 0,0, 1,0, 1,1]);
@@ -55,10 +62,18 @@ export class MyBarn extends CGFobject {
         // tras
         this.scene.pushMatrix();
         this.scene.translate(0, 0, -4);
-        this.scene.scale(4, 3, 1);
+        this.scene.scale(4, 3, -1);
         this.woodMaterial.apply();
         this.Quad.display();
+        //janela
+        this.scene.pushMatrix();
+        this.scene.translate(0.39, 1.1, 0.01);
+        this.scene.scale(0.18, 0.22, 0);
+        this.windowMaterial.apply();
+        this.Quad.display();
         this.scene.popMatrix();
+        this.scene.popMatrix();
+
 
         // baixo
         this.scene.pushMatrix();
@@ -80,9 +95,9 @@ export class MyBarn extends CGFobject {
 
         // direita
         this.scene.pushMatrix();
-        this.scene.translate(0, 0, 0);
+        this.scene.translate(0, 0, -4);
         this.scene.scale(3, 3, 4);
-        this.scene.rotate(Math.PI/2, 0, 1, 0);
+        this.scene.rotate(-Math.PI/2, 0, 1, 0);
         this.woodMaterial.apply();
         this.Quad.display();
         this.scene.popMatrix();
@@ -103,19 +118,18 @@ export class MyBarn extends CGFobject {
         // tras
         this.scene.pushMatrix();
         this.scene.translate(0, 3, -4);
-        this.scene.scale(2, 2, 1);
+        this.scene.scale(2, 2, -1);
         this.roofMaterial.apply();
         this.Triangle.display();
         this.scene.popMatrix();
 
         // telhado direita
         this.scene.pushMatrix();
-        this.scene.translate(2, 5, -4);
+        this.scene.translate(2, 5, 0);
         this.scene.scale(Math.sqrt(8), 3, 4);
-        this.scene.rotate(Math.PI/2, 1, 0, 0);
-        this.scene.rotate(-Math.atan(0.942), 0, 1, 0);
-        // repetir textura no telhado (u x4, v x2)
-        this.Quad.updateTexCoords([0,4, 0,0, 2,0, 4,2]);
+        this.scene.rotate(-Math.PI/2, 1, 0, 0);
+        this.scene.rotate(Math.atan(0.942), 0, 1, 0);
+        this.Quad.updateTexCoords([0,4, 0,0, 4,0, 4,4]);
         this.roofMaterial.apply();
         this.Quad.display();
         this.scene.popMatrix();
@@ -127,7 +141,7 @@ export class MyBarn extends CGFobject {
         this.scene.rotate(Math.PI/2, 1, 0, 0);
         this.scene.rotate(-(Math.atan(1.061) + Math.PI/2), 0, 1, 0);
         // manter mesma repetição no outro lado
-        this.Quad.updateTexCoords([0,4, 0,0, 2,0, 2,4]);
+        this.Quad.updateTexCoords([0,4, 0,0, 4,0, 4,4]);
         this.roofMaterial.apply();
         this.Quad.display();
         this.scene.popMatrix();
