e871f78461744b5e68dc563c4d67416af99337b2
Joao Junior <up202306719@up.pt>
2026-05-29 15:36:10 +0100

    respawn hales

--- project/modelos/MyGround.js
+++ project/modelos/MyGround.js
@@ -188,8 +188,8 @@ export class MyGround extends CGFobject {
         this.material.setDiffuse(1, 1, 1, 1);
         this.material.setSpecular(0.05, 0.05, 0.05, 1);
         this.material.setShininess(5);
-        //this.material.loadTexture("images/ground.jpg");
-        //this.material.setTextureWrap('REPEAT', 'REPEAT');
+        this.material.loadTexture("images/ground.jpg");
+        this.material.setTextureWrap('REPEAT', 'REPEAT');
 
         // Shader: usa uSampler (unit 0, padrão CGF) + blending por cor para dirt e caminho
         /*this.shader = new CGFshader(
